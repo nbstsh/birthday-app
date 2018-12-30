aaeaf41437bf82e1879ee00d449ee974728b23cb
nbstsh <nbstsh@gmail.com>
2018-12-30 18:48:37 +0700

    create function to generate dates-array and characters-object sorted by birthday

---
+++ src/dates.js
@@ -0,0 +1,22 @@
+const dates = []
+
+const generateDates = (sortedCharacters) => {
+    sortedCharacters.forEach((character) => {
+        const birthday = character.birthday.split('/')
+        const dateObj = dates.find((date) => date.month === birthday[0] && date.date === birthday[1])
+
+        if (dateObj) {
+            dateObj.characters.push(character)
+        } else {
+            dates.push({
+                month: birthday[0],
+                date: birthday[1],
+                characters: [character]
+            })
+        }
+    })
+    return dates
+}
+
+
+export { generateDates }
