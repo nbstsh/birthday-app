eccf6d5a9924b71fd6ff6b83c036b46a7bc35638
nbstsh <nbstsh@gmail.com>
2019-01-18 04:41:54 +0800

    create date-text/month-date animation

--- src/character-form.js
+++ src/character-form.js
@@ -5,22 +5,11 @@ const selector = {
     monthInput: '#character-form-month',
     dateInput: '#character-form-date',
     monthNumber: '.date-form__month-number',
-    dateNumber: '.date-form__date-number'
+    dateNumber: '.date-form__date-number',
+    monthText: '#selected-month-text',
+    dateText: '#selected-date-text'
 }
 
-
-// add event to take class from existing element and add class to new element
-const addMoveClassEvent = (el, targetSelector, addedClassName, callback) => {
-    el.addEventListener('click', (e) => {
-        const clickedEl = document.querySelector(`${targetSelector}.${addedClassName}`)
-        if (clickedEl){
-            clickedEl.classList.remove(addedClassName)
-        }
-
-        e.target.classList.add(addedClassName)
-        callback()
-    })
-}
 
 // remove a class and add the class to target element
 const moveClass = (targetEl, targetSelector, addedClassName) => {
@@ -32,14 +21,28 @@ const moveClass = (targetEl, targetSelector, addedClassName) => {
     targetEl.classList.add(addedClassName)
 }
 
+// render month number of date-form
 const renderMonthNumber = (numEl) => {
     moveClass(numEl, selector.monthNumber, 'clicked')
     setInputValue(selector.monthInput, numEl.textContent)
 }
 
+// render date number of date-form
 const renderDateNumber = (numEl) => {
     moveClass(numEl, selector.dateNumber, 'clicked')
     setInputValue(selector.dateInput, numEl.textContent)
+}
+
+const initDisplayText = (selector, text) => {
+    document.querySelector(selector).textContent = text
+}
+
+const initMonthText = (number) => {
+    initDisplayText(selector.monthText, `${number}月`)
+}
+
+const initDateText = (number) => {
+    initDisplayText(selector.dateText, `${number}月`)
 }
 
 
@@ -47,6 +50,7 @@ const renderDateNumber = (numEl) => {
 document.querySelectorAll(selector.monthNumber).forEach((numEl) => {
     numEl.addEventListener('click', (e) => {
         renderMonthNumber(numEl)
+        initMonthText(e.target.dataset.value)
     })
 })
 
@@ -54,6 +58,7 @@ document.querySelectorAll(selector.monthNumber).forEach((numEl) => {
 document.querySelectorAll(selector.dateNumber).forEach((numEl) => {
     numEl.addEventListener('click', (e) => {
         renderDateNumber(numEl)
+        initDateText(e.target.dataset.value)
     })
 })
 
@@ -61,11 +66,13 @@ document.querySelectorAll(selector.dateNumber).forEach((numEl) => {
 document.querySelector(selector.monthInput).addEventListener('input', (e) => {
     const numEl = document.querySelector(`${selector.monthNumber}[data-value="${e.target.value}"]`)
     renderMonthNumber(numEl)
+    initMonthText(e.target.value)
 })
 
 // date-form date-input event
 document.querySelector(selector.dateInput).addEventListener('input', (e) => {
     const numEl = document.querySelector(`${selector.dateNumber}[data-value="${e.target.value}"]`)
     renderDateNumber(numEl)
+    initDateText(e.target.value)
 })
 
